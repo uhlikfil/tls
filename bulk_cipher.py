c826ff411b8fcfbc4aef2c552f2446e930c1c9ab
dkhikhlukha <d.hihluha@gmail.com>
2019-11-07 10:43:36 +0100

    Update bulk_cipher.py

--- bulk_cipher.py
+++ bulk_cipher.py
@@ -2,6 +2,10 @@ import random
 import string
 import os
 
+BLOCK_SIZE = 16
+
+key = os.urandom(BLOCK_SIZE)
+iv = os.urandom(BLOCK_SIZE)
 msg = ''.join(random.choice(string.ascii_lowercase) for i in range(1024))
 assert decrypt(key, iv, encrypt(key, iv, msg)) == msg
 
