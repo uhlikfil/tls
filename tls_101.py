99b2723c3949e811900e4e51a727a8b5fd08b660
Danila Khikhlukha <dkhikhlu@cisco.com>
2019-11-07 09:35:12 +0100

    Initial commit

---
+++ tls_101.py
@@ -0,0 +1,21 @@
+
+alice = Agent("I'M 5UppER Kewl h4zKEr")
+bob = Agent()
+mallory = MITM()
+
+# Alice has da message, Bob doesn't
+assert alice.msg
+assert not bob.msg
+
+# Negotiate parameters publicly
+bob.receive_public_data(*alice.send_public_data())
+alice.receive_public_data(*bob.send_public_data())
+
+# Exchange keys publicly
+bob.receive_public_key(alice.send_public_key())
+alice.receive_public_key(bob.send_public_key())
+
+# Pass da message
+bob.receive_message(alice.send_message())
+# Bob has it now
+assert alice.msg == bob.msg
